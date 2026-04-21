3bf404312597aa3d9c99d116a08ffd94f2e7c9b9
subhakshanchakraborty <subhakshan.chakraborty@tudip.com>
2026-04-21 19:27:30 +0530

    changes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from starlette.responses import FileResponse
+from prometheus_client import start_http_server
 
 app = FastAPI()
+
+# Start Prometheus metrics server (runs on separate port)
+start_http_server(8001)
 
 @app.get('/')
 def home():
